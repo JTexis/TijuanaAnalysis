5c2c7969ba8be136cd3c3bac89a9d1e5518c0a91
JavierTexis <robertotsux@gmail.com>
2023-09-19 23:40:35 -0700

    First commit

---
+++ collection.py
@@ -0,0 +1,39 @@
+columns_to_drop = [
+'fecha_alta',
+'longitud',
+'latitud',
+'tipoUniEco',
+'ageb',
+'cve_loc',
+'cve_mun',
+'cve_ent',
+'num_local',
+'nom_CenCom',
+'tipoCenCom',
+'letra_int',
+'numero_int',
+'edificio_e',
+'edificio',
+'nom_v_e_3',
+'tipo_v_e_3',
+'nom_v_e_2',
+'tipo_v_e_2',
+'nom_v_e_1',
+'tipo_v_e_1',
+'nom_vial',
+'tipo_vial',
+'clee',
+'id',
+'municipio'
+]
+
+cat_to_num_perocu = {
+    '0 a 5 personas':1,
+    '6 a 10 personas':6,
+    '11 a 30 personas':11,
+    '31 a 50 personas':31,
+    '51 a 100 personas':51,
+    '101 a 250 personas':101,
+    '251 y más personas':251
+}
+
